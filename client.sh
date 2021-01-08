2ae4e1f1d94d44c49dc658959983eb6485594cff
MuzafarWani <76955375+MuzafarWani@users.noreply.github.com>
2021-01-08 15:49:03 +0530

    Add files via upload

---
+++ client.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+#clientprogram ip_address_server filename_to_be_sent
+./tcpclient 127.0.0.1 file11
---
+++ server.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+#serverprogram ip_address_server filename_to_be_created
+./tcpserver 127.0.0.1 file100
