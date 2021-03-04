4cd578e7654578b7bbe88ea1b6906a225b5da22e
MuzafarWani <76955375+MuzafarWani@users.noreply.github.com>
2021-03-04 09:28:32 +0530

    Add files via upload

---
+++ plots/multiple_client_graph.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+
+X, Y = [], []
+for line in open('iperf_multiple_client.txt', 'r'):
+  values = [float(s) for s in line.split()]
+  X.append(values[0])
+  Y.append(values[1])
+ 
+plt.plot(X, Y)
+X, Y = [], []
+for line in open('netperf_multiple_client.txt', 'r'):
+  values = [float(s) for s in line.split()]
+  X.append(values[0])
+  Y.append(values[1])
+  
+plt.plot(X, Y)  
+
+X, Y = [], []
+for line in open('multiple_client_code_data.txt', 'r'):
+  values = [float(s) for s in line.split()]
+  X.append(values[0])
+  Y.append(values[1])
+  
+plt.plot(X, Y)
+
+plt.xlabel('Data Transfered(MB)') 
+plt.ylabel('Bandwidith(MB/s)') 
+
+
+plt.title("Bandwidth varaition measured with sends from multiple clients") 
+
+plt.ylim(ymin=0)
+
+plt.legend(["iperf", "netperf", "code measured"], loc ="lower right") 
+plt.show()
