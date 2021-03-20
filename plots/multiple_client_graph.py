023b0b530f5dc693323c2b953eece9194baf12f8
MuzafarWani <76955375+MuzafarWani@users.noreply.github.com>
2021-03-20 10:41:29 +0530

    Update multiple_client_graph.py

--- plots/multiple_client_graph.py
+++ plots/multiple_client_graph.py
@@ -6,14 +6,14 @@ for line in open('iperf_multiple_client.txt', 'r'):
   X.append(values[0])
   Y.append(values[1])
  
-plt.plot(X, Y)
+plt.plot(X, Y,"-o")
 X, Y = [], []
 for line in open('netperf_multiple_client.txt', 'r'):
   values = [float(s) for s in line.split()]
   X.append(values[0])
   Y.append(values[1])
   
-plt.plot(X, Y)  
+plt.plot(X, Y,"-o")
 
 X, Y = [], []
 for line in open('multiple_client_code_data.txt', 'r'):
@@ -21,7 +21,7 @@ for line in open('multiple_client_code_data.txt', 'r'):
   X.append(values[0])
   Y.append(values[1])
   
-plt.plot(X, Y)
+plt.plot(X, Y,"-o")
 
 plt.xlabel('Data Transfered(MB)') 
 plt.ylabel('Bandwidith(MB/s)') 
